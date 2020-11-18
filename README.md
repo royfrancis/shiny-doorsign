14c0dd4136e8857b8213c1906c8f6b49072121e0
Roy Francis <roymathewfrancis@gmail.com>
2020-11-18 16:08:56 +0100

    v1.1.0
    - Formatting & style changes,
    - Image square crop and circular mask
    - shinyBS dependency removed
    -

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Click [here](https://roymf.shinyapps.io/doorsign/) to access an online instance 
 ### Run using docker
 
 ```
-docker run --rm -p 8787:8787 royfrancis/shiny-doorsign:v1.0.0
+docker run --rm -p 8787:8787 royfrancis/shiny-doorsign:v1.1.0
 ```
 
 The app should be available through a web browser at `http://0.0.0.0:8787`.
@@ -25,9 +25,9 @@ The app should be available through a web browser at `http://0.0.0.0:8787`.
 Install the following R packages:
 
 ```
-install.packages(c(Cairo, curl, ggplot2, magick, png, shiny, shinyBS, shinythemes, showtext))
+install.packages(c(Cairo, curl, ggplot2, magick, png, shiny, shinythemes, showtext))
 ```
 
 This repo is not an R package. In the root directory of this repo, run app using `shiny::runApp("app.R")`.
 
-2020 | Roy Francis
+2020 • Roy Francis
--- app.R
+++ app.R
@@ -7,59 +7,66 @@ source("functions.R")
 # UI ---------------------------------------------------------------------------
 
 ui <- fluidPage(theme=shinytheme("flatly"),
-  fixedRow(
-      column(12,style="margin:15px;",
-             fluidRow(style="margin-bottom:10px;",
-                      span(tags$img(src='nbis.png',style="height:18px;"),style="vertical-align:top;display:inline-block;"),
-                      span(tags$h4("•",style="margin:0px;margin-left:6px;margin-right:6px;"),style="vertical-align:top;display:inline-block;"),
-                      span(tags$h4(strong("Door Sign"),style="margin:0px;"),style="vertical-align:middle;display:inline-block;")
-             ),
-    fixedRow(
-    column(3,style="max-width:300px;background:#ebedef;padding-top:15px;padding-bottom:15px;border-radius:4px;",
-      fluidRow(
-        column(12,
-               textInput("in_text_name","Name",value="John Doe"),
-               textInput("in_text_title","Title",value="Bioinformatician; NBIS"),
-               textInput("in_text_dept","Department",value="Dept. of Cell and Molecular Biology (ICM)"),
-               textInput("in_text_email","Email",value="john.doe@nbis.se,john.doe@scilifelab.se"),
-               textInput("in_text_phone","Phone",value="0730000000")
-        )
-      ),
-      fluidRow(
-        column(12,
-               style="padding-top:5px;padding-bottom:10px",
-                      icon("info-circle",class="fa-lg"),
-                      helpText("Multiple emails or phone can be entered using comma (,).",style="display:inline;")
-               )
-        ),
-      fluidRow(
-        column(12,
-               fileInput("in_img_profile","Profile image")
-        )
-      ),
-      sliderInput("in_pos_y_text","Text position",min=0.10,max=0.90,value=0.56,step=0.01),
-      sliderInput("in_img_profile_pos_y","Profile image position",min=0.01,max=0.40,value=0.12,step=0.01),
-      sliderInput("in_img_profile_width","Profile image size",min=0.10,max=0.80,value=0.40,step=0.01),
-      downloadButton("btn_download","Download"),
-      tags$hr(),
-      helpText(paste0("2020 | Roy Francis | Version: ",fn_version()))
-    ),
-    column(6,style="max-width:450px;min-width:400px;padding-top:15px;padding-bottom:15px;border-radius:4px;",
-      sliderInput("in_scale","Image preview scale",min=1,max=6,step=0.2,value=3),
-      fluidRow(
-        column(12,
-               style="padding-top:5px;padding-bottom:10px",
-                      icon("info-circle",class="fa-lg"),
-                      helpText("Scale only controls preview here and does not affect download.",style="display:inline;")
-               )
-        ),
-      tags$br(),
-      imageOutput("out_plot")
-      #verbatimTextOutput("out_display")
-    )
-    )
-  )
-)
+                tags$head(tags$link(rel="stylesheet",type="text/css",href="styles.css")),
+                fixedRow(
+                  column(12,style="margin:15px;",
+                         fluidRow(style="margin-bottom:10px;",
+                                  span(tags$img(src='nbis.png',style="height:18px;"),style="vertical-align:top;display:inline-block;"),
+                                  span(tags$h4("•",style="margin:0px;margin-left:6px;margin-right:6px;"),style="vertical-align:top;display:inline-block;"),
+                                  span(tags$h4(strong("Door Sign"),style="margin:0px;"),style="vertical-align:middle;display:inline-block;")
+                         ),
+                         fixedRow(
+                           column(3,style="max-width:300px;background:#ebedef;padding-top:15px;padding-bottom:15px;border-radius:4px;",
+                                  fluidRow(
+                                    column(12,style="margin-bottom:10px;",
+                                           textInput("in_text_name",NULL,value="John Doe",placeholder="Name"),
+                                           textInput("in_text_title",NULL,value="Bioinformatician; NBIS",placeholder="Job title"),
+                                           textInput("in_text_dept",NULL,value="Dept. of Cell and Molecular Biology (ICM)",placeholder="Department"),
+                                           textInput("in_text_email",NULL,value="john.doe@nbis.se,john.doe@scilifelab.se",placeholder="Email(s)"),
+                                           textInput("in_text_phone",NULL,value="0730000000",placeholder="Phone"),
+                                           HTML('<div class="help-note"><i class="fas fa-info-circle"></i>  Enter multiple email/phone using comma (,).</div>')
+                                    )
+                                  ),
+                                  sliderInput("in_pos_y_text","Text vertical position",min=0.10,max=0.90,value=0.56,step=0.01),
+                                  fluidRow(
+                                    column(12,style="margin-top:15px;",
+                                           fileInput("in_im_profile","Profile image",multiple=FALSE)
+                                    )
+                                  ),
+                                  fluidRow(style="margin-bottom:10px;margin-top:10px;",
+                                           column(6,style=list("padding-right: 3px;"),
+                                                  numericInput("in_im_profile_width","Image size",min=0.10,max=0.80,value=0.40,step=0.01)
+                                           ),
+                                           column(6,style=list("padding-left: 3px;"),
+                                                  numericInput("in_im_profile_offset_y","Image position",min=0.01,max=0.40,value=0.20,step=0.01)
+                                           )
+                                  ),
+                                  fluidRow(style="margin-bottom:15px;margin-top:5px;",
+                                           column(6,style=list("padding-right: 3px;"),
+                                                  numericInput("in_nudge_x","Image nudge x",min=0,max=50,value=0,step=1)
+                                           ),
+                                           column(6,style=list("padding-left: 3px;"),
+                                                  numericInput("in_nudge_y","Image nudge y",min=0,max=50,value=0,step=1)
+                                           )
+                                  ),
+                                  div(style="margin-top:25px;margin-bottom:20px;",downloadButton("btn_download","Download")),
+                                  div(style="font-size:0.8em;",paste0(format(Sys.time(),'%Y'),' • Roy Francis • Version: ',fn_version()))
+                           ),
+                           column(6,style="max-width:450px;min-width:400px;padding-top:15px;padding-bottom:15px;border-radius:4px;",
+                                  sliderInput("in_scale","Image preview scale",min=1,max=6,step=0.2,value=3,width="100%"),
+                                  fluidRow(
+                                    column(12,style="padding-top:5px;padding-bottom:10px",
+                                           HTML('<div class="help-note"><i class="fas fa-info-circle"></i>  Scale controls preview below and does not affect download.</div>')
+                                    )
+                                  ),
+                                  div(class="img-output",
+                                      imageOutput("out_plot",width="auto",height="auto")
+                                  )
+                                  #verbatimTextOutput("out_display")
+                           )
+                         )
+                  )
+                )
 )
 
 # SERVER -----------------------------------------------------------------------
@@ -81,16 +88,13 @@ server <- function(input, output, session) {
           tags$b("Label 1"),
           fluidRow(
             column(4,style=list("padding-right: 3px;"),
-                   numericInput("in_label1_size",label="Size",value=8,min=0,max=20,step=0.5),
-                   shinyBS::bsTooltip(id="in_label1_size",title="Size of label 1. A value between 0 and 20.0.",placement="top",trigger="hover")
+                   numericInput("in_label1_size",label="Size",value=8,min=0,max=20,step=0.5)
             ),
             column(4,style=list("padding-right: 3px; padding-left: 3px;"),
-                   numericInput("in_label1_x",label="Hor pos",value=0.5,min=0,max=1,step=0.02),
-                   shinyBS::bsTooltip(id="in_label1_x",title="Horizontal position of label 1. A value between 0.00 and 1.00 denoting left to right.",placement="center",trigger="hover")
+                   numericInput("in_label1_x",label="Hor pos",value=0.5,min=0,max=1,step=0.02)
             ),
             column(4,style=list("padding-left: 3px;"),
-                   numericInput("in_label1_y",label="Ver pos",value=0.54,min=0,max=1,step=0.02),
-                   shinyBS::bsTooltip(id="in_label1_y",title="Vertical position of label 1. A value between 0.00 and 1.00 denoting bottom to top.",placement="right",trigger="hover")
+                   numericInput("in_label1_y",label="Ver pos",value=0.54,min=0,max=1,step=0.02)
             )
           )
         )
@@ -121,9 +125,12 @@ server <- function(input, output, session) {
     if(is.null(input$in_pos_x)){pos_x <- 0.12}else{pos_x <- input$in_pos_x}
     if(is.null(input$in_pos_y_text)){pos_y_text <- 0.56}else{pos_y_text <- input$in_pos_y_text}
     if(is.null(input$in_line_spacing)){line_spacing <- 0.042}else{line_spacing <- input$in_line_spacing}
-    if(is.null(input$in_img_profile_width)){img_profile_width <- 0.4}else{img_profile_width <- input$in_img_profile_width}
-    if(is.null(input$in_img_profile_pos_y)){img_profile_pos_y <- 0.37}else{img_profile_pos_y <- input$in_img_profile_pos_y}
+    if(is.null(input$in_im_profile_width)){im_profile_width <- 0.4}else{im_profile_width <- input$in_im_profile_width}
+    if(is.null(input$in_im_profile_offset_y)){im_profile_offset_y <- 0.20}else{im_profile_offset_y <- input$in_im_profile_offset_y}
 
+    if(is.null(input$in_nudge_x)){nudge_x <- 0}else{nudge_x <- input$in_nudge_x}
+    if(is.null(input$in_nudge_y)){nudge_y <- 0}else{nudge_y <- input$in_nudge_y}
+    
     # validation ---------------------------------------------------------------
     
     fn_val <- function(x) {
@@ -140,7 +147,7 @@ server <- function(input, output, session) {
     
     dfr <- data.frame(label=c(text_name,text_title,text_dept),
                       type=c("name","title","dept"))
-    dfr1 <- data.frame(label=unlist(strsplit(text_email,",")))
+    dfr1 <- data.frame(label=trimws(unlist(strsplit(text_email,","))))
     dfr1$type <- "email"
     dfr2 <- data.frame(label=unlist(strsplit(text_phone,",")))
     dfr2$type <- "phone"
@@ -149,19 +156,30 @@ server <- function(input, output, session) {
     logo_right <- png::readPNG("www/scilifelab.png")
     logo_left <- png::readPNG("www/nbis.png")
 
-    if(is.null(input$in_img_profile)) {
-      img_profile <- png::readPNG("www/profile.png")
+    fn_validate_im <- function(x){
+      if(!is.null(x)){
+        y <- tolower(sub("^.+[.]","",basename(x$datapath)))
+        if(!y %in% c("jpg","png","jpeg","gif")) return("Image must be one of JPG/JPEG, PNG or GIF formats.")
+        if((x$size/1024/1024)>2) return("Image must be less than 2 MB in size.")
+      }
+    }
+    
+    validate(fn_validate_im(input$in_im_profile))
+    
+    if(is.null(input$in_im_profile)) {
+      im_profile <- png::readPNG("www/profile.png")
     }else{
-      magick::image_write(
-        magick::image_convert(magick::image_read(input$in_img_profile$datapath),"png"),
-        path=file.path(store$epath,"image.png"),format="png")
-      img_profile <- png::readPNG(file.path(store$epath,"image.png"))
+      # read image, convert to png, square aspect, export as png
+      im_final <- fn_circ_image(im=magick::image_read(input$in_im_profile$datapath),nudge_x=nudge_x,nudge_y=nudge_y)
+      
+      magick::image_write(im_final,path=file.path(store$epath,"image.png"),format="png")
+      im_profile <- png::readPNG(file.path(store$epath,"image.png"))
     }
     
     f <- "Merriweather"
 
-    return(list(dfr=dfr,logo_right=logo_right,logo_left=logo_left,img_profile=img_profile,
-                pos_y_text=pos_y_text,img_profile_pos_y=img_profile_pos_y,img_profile_width=img_profile_width))
+    return(list(dfr=dfr,logo_right=logo_right,logo_left=logo_left,im_profile=im_profile,
+                pos_y_text=pos_y_text,im_profile_offset_y=im_profile_offset_y,im_profile_width=im_profile_width))
   })
 
   ## OUT: out_plot ------------------------------------------------------------
@@ -174,8 +192,8 @@ server <- function(input, output, session) {
     
     progress1$set(message="Generating figure...", value=40)
     
-    plot_doorsign(dfr=p$dfr,img_profile=p$img_profile,logo_left=p$logo_left,logo_right=p$logo_right,
-                  pos_y_text=p$pos_y_text,img_profile_pos_y=p$img_profile_pos_y,img_profile_width=p$img_profile_width,
+    plot_doorsign(dfr=p$dfr,im_profile=p$im_profile,logo_left=p$logo_left,logo_right=p$logo_right,
+                  pos_y_text=p$pos_y_text,im_profile_offset_y=p$im_profile_offset_y,im_profile_width=p$im_profile_width,
 		  path_export=store$epath,format_export="png")
     progress1$set(message="Completed.", value=100)
     progress1$close()
@@ -231,8 +249,8 @@ server <- function(input, output, session) {
 
   fn_download <- function(){
     p <- fn_params()
-    plot_doorsign(dfr=p$dfr,img_profile=p$img_profile,logo_left=p$logo_left,logo_right=p$logo_right,
-                  pos_y_text=p$pos_y_text,img_profile_pos_y=p$img_profile_pos_y,img_profile_width=p$img_profile_width,
+    plot_doorsign(dfr=p$dfr,im_profile=p$im_profile,logo_left=p$logo_left,logo_right=p$logo_right,
+                  pos_y_text=p$pos_y_text,im_profile_offset_y=p$im_profile_offset_y,im_profile_width=p$im_profile_width,
 		  path_export=store$epath,format_export="pdf")
   }
 
